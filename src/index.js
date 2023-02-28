c7d261d06057594ee0742c6719a1fb51272b9ebe
Maksym Ostapchuk <gadget.add@gmail.com>
2023-02-28 14:17:28 +0200

    first homework commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './css/styles.css';
+import axios from 'axios';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import InfiniteScroll from 'infinite-scroll';
